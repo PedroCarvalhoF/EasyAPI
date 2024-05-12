8c858bc7d92c04433d8607308183f9aa90e847d5
PedroCarvalhoF <operacoes.pedro@gmail.com>
2024-05-11 22:18:47 -0300

    refactor controlers PontoVenda/PrecoProduto

--- src/Api.Application/Controllers/PrecosProdutosController.cs
+++ src/Api.Application/Controllers/PrecosProdutosController.cs
@@ -1,5 +1,7 @@
+using Api.Domain.Dtos.PontoVendaDtos;
 using Api.Domain.Dtos.PrecoProdutoDtos;
 using Api.Domain.Interfaces.Services.PrecoProdutoService;
+using Domain.Dtos;
 using Domain.ExceptionsPersonalizadas;
 using Microsoft.AspNetCore.Mvc;
 
@@ -17,7 +19,7 @@ namespace Api.Application.Controllers
         }
 
         [HttpGet("precos-produtos")]
-        public async Task<ActionResult<IEnumerable<PrecoProdutoDto>>> GetAll()
+        public async Task<ActionResult<ResponseDto<List<PrecoProdutoDto>>>> GetAll()
         {
             try
             {
