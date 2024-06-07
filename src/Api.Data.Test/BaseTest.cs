226c23604d9f8fe4d25307a45c236131f71ee816
PedroCarvalhoF <operacoes.pedro@gmail.com>
2024-06-07 04:07:44 -0300

    #Data.Test Create

---
+++ src/Api.Data.Test/BaseTest.cs
@@ -0,0 +1,46 @@
+﻿using Api.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data.Test
+{
+    public abstract class BaseTest
+    {
+        public BaseTest()
+        {
+
+        }
+    }
+
+    public class DbTeste : IDisposable
+    {
+        private string dataBaseName = $"dbApiTest_{Guid.NewGuid().ToString().Replace("-", string.Empty)}";
+        public ServiceProvider ServiceProvider { get; private set; }
+
+        public DbTeste()
+        {
+            MySqlServerVersion serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
+            string? desenvolvimento = $"Server=localhost;Port=3306;DataBase={dataBaseName};Uid=root;password=010203;";
+            
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<MyContext>(o =>
+                o.UseMySql(desenvolvimento, serverVersion),
+                  ServiceLifetime.Transient
+            );
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+        }
+
+        public void Dispose()
+        {
+            using (var context = ServiceProvider.GetService<MyContext>())
+            {
+                context.Database.EnsureDeleted();
+            }
+        }
+    }
+}
---
+++ src/Api.Data.Test/Pessoa/PessoaFuncionario/PessoaFuncionarioCrudCompleto.cs
@@ -0,0 +1,43 @@
+﻿using Api.Data.Context;
+using Data.Implementations.Pessoas.PessoaImplentetacoes;
+using Data.Migrations;
+using Domain.Entities.Pessoa.Pessoas;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Data.Test.Pessoa.PessoaFuncionario
+{
+    public class PessoaFuncionarioCrudCompleto : BaseTest, IClassFixture<DbTeste>
+    {
+        private ServiceProvider _serviceProvide;
+
+        public PessoaFuncionarioCrudCompleto(DbTeste dbTeste)
+        {
+            _serviceProvide = dbTeste.ServiceProvider;
+        }
+
+        [Fact(DisplayName = "CRUD de Pessoa Funcionario")]
+        [Trait("CRUD", "PessoaFuncionarioEntity")]
+        public async Task E_Possivel_Realizar_CRUD_Usuario()
+        {
+            using (var context = _serviceProvide.GetService<MyContext>())
+            {
+                PessoaImplementacao _pessoaRepoository = new PessoaImplementacao(context);
+                var pessoa = new PessoaEntity
+                {
+                    Habilitado = true,
+                    CpfCnpj = "123123123-12",
+                    CreateAt = DateTime.Now,
+                    DataNascimentoFundacao = DateTime.Now,
+                    Id = Guid.NewGuid(),
+                    NomeNomeFantasia = Faker.Name.First(),
+                    PessoaTipo = Domain.Enuns.Pessoas.PessoaTipoEnum.PessoaFisica,
+                    RgIE = Faker.RandomNumber.Next(10, 20).ToString(),
+                    SobreNomeRazaoSocial = Faker.Name.Last(),
+                    UpdateAt = DateTime.Now,
+                };
+
+                var resultPessoa = await _pessoaRepoository.InsertAsync(pessoa);
+            }
+        }
+    }
+}
