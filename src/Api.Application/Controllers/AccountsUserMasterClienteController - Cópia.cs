09087c23ac4e20f0f1257fdc2e3ee00b877312c9
PedroCarvalhoF <operacoes.pedro@gmail.com>
2024-06-17 13:55:08 -0300

    save

---
+++ src/Api.Application/Controllers/AccountsUserMasterClienteController - Cópia.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Application.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize]
+    public class AccountsUserMasterClienteController : ControllerBase
+    {
+    }
+}
