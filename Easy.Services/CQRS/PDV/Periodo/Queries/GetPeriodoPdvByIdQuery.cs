c4ac03d8001beb35116c8656914ae5bdc458a54a
PedroCarvalhoF <operacoes.pedro@gmail.com>
2024-06-29 02:56:10 -0300

    feat: PeriodoPdv - GetById

--- Easy.Services/CQRS/PDV/Periodo/Queries/GetPeriodoPdvByIdQuery.cs
+++ Easy.Services/CQRS/PDV/Periodo/Queries/GetPeriodoPdvByIdQuery.cs
@@ -1,17 +1,27 @@
-﻿using Easy.Services.DTOs;
+﻿using Easy.Domain.Intefaces;
+using Easy.Services.DTOs;
 using MediatR;
 
 namespace Easy.Services.CQRS.PDV.Periodo.Queries;
 
 public class GetPeriodoPdvByIdQuery : BaseCommands
 {
-    public Guid Id { get; set; }
+	public Guid Id { get; set; }
 
-    public class GetPeriodoPdvByIdQueryHandler : IRequestHandler<GetPeriodoPdvByIdQuery, RequestResult>
-    {       
-        public Task<RequestResult> Handle(GetPeriodoPdvByIdQuery request, CancellationToken cancellationToken)
-        {
-            throw new NotImplementedException();
-        }
-    }
+	public class GetPeriodoPdvByIdQueryHandler(IUnitOfWork _repository) : IRequestHandler<GetPeriodoPdvByIdQuery, RequestResult>
+	{
+		public async Task<RequestResult> Handle(GetPeriodoPdvByIdQuery request, CancellationToken cancellationToken)
+		{
+			try
+			{
+				var periodoEntity = await _repository.PeriodoPdvRepository.SelectAsync(request.Id, request.GetFiltro());
+				return new RequestResult().Ok(periodoEntity);
+			}
+			catch (Exception ex)
+			{
+
+				return new RequestResult().BadRequest(ex.Message);
+			}
+		}
+	}
 }
